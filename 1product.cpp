51426069050bfdc5bf4986531d39db03f4b9181d
gautamgc17 <71714942+gautamgc17@users.noreply.github.com>
2020-11-17 00:16:49 +0530

    Add files via upload

---
+++ 1product.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+	float x,y,product;
+	cout<<"Enter two numbers to find product\n";
+	cin>>x>>y;
+	product = x*y;
+	cout<<"The Product is: "<<product;
+	return 0;
+}
