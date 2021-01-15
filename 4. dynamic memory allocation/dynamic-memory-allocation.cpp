f72dc91cf5dd9e4f75f775c84984128017dcf0c8
gautamgc17 <71714942+gautamgc17@users.noreply.github.com>
2021-01-15 15:53:16 +0530

    Add files via upload

---
+++ 4. dynamic memory allocation/dynamic-memory-allocation.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	int* pi = new int;     // 8 bytes allocated for pi in stack memory and 4 bytes created for integer in heap memory
+	*pi = 10;
+	cout<< *pi <<endl;
+	
+	delete pi;
+	
+	char *pc = new char;
+	*pc = 'A';
+	cout<< *pc <<endl;
+	
+	delete pc;
+	
+	int n;
+	cout<<"Enter num of elements: ";
+	cin>>n;
+	
+	int *pa = new int[n];
+	
+	for(int i=0; i<n; i++){
+		cin>>pa[i];
+	}
+	
+	int max = -1;
+	for(int i=0; i<n; i++){
+		if (pa[i] > max){
+			max = pa[i];
+		}
+	}
+	cout<< "Max num in array:"<< max;
+	
+	delete [] pa;
+	
+	return 0;
+}
