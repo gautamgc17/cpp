e41bab31fc98a837e93a0973c6e4e5c6b03acc2b
gautamgc17 <71714942+gautamgc17@users.noreply.github.com>
2021-01-15 18:48:03 +0530

    Add files via upload

--- 4. dynamic memory allocation/allocating-2D-dynamic-arrays.cpp
+++ 4. dynamic memory allocation/allocating-2D-dynamic-arrays.cpp
@@ -5,29 +5,36 @@ int main(){
 	int r , c;
 	cin>>r>>c;
 	
-	int **p = new int*[r];        
+	int **arr;
 	
+	// Create an array of row heads 
+	arr = new int *[r];
+	
+	// Create a 2-D array       
 	for(int i=0; i<r; i++){
 		
-		p[i] = new int[c];
-		
-		for(int j=0; j<c; j++){
+		arr[i] = new int[c];          // each element stores adddress of new array with size = no of columns
+    }
+    
+    int val = 1;
+    
+    for(int i=0; i<r; i++){
+    	for(int j=0; j<c; j++){
+    		
+    		arr[i][j] = val;
+			val++;
 			
-			cin>>p[i][j];
-		}
-	}
-	
-	for(int i=0;i<r;i++){
-		for(int j=0;j<c;j++){
-			cout<<p[i][j]<<" ";
+			cout<< arr[i][j] <<	" ";
 		}
 		cout<<endl;
 	}
-	
+
+    // Release memory spaces
+    
 	for(int i=0; i<r; i++){
-		delete [] p[i];
+		delete [] arr[i];
 	}
 	
-	delete [] p;
+	delete [] arr;
 	
 }
