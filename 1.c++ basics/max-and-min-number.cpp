e73e2e5f2631e7c808322d0d02fee4d900b4590d
gautamgc17 <71714942+gautamgc17@users.noreply.github.com>
2021-01-16 19:37:41 +0530

    Add files via upload

---
+++ 1.c++ basics/max-and-min-number.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+int main(){
+	
+	int n;
+	cout<<"Enter upper limit: ";
+	cin>>n;
+	
+	int max_so_far = INT_MIN;
+	int min_so_far = INT_MAX;
+	    
+	for(int i=0; i<n; i++){
+		int no;
+		cin>>no;
+	    
+	    if(no > max_so_far){
+	    	max_so_far = no;
+		}
+		if(no < min_so_far){
+			min_so_far = no;	
+		}
+	}
+	
+	cout<<"Max Number: "<<max_so_far<<endl;
+	cout<<"Min Number: "<<min_so_far<<endl;
+		
+	return 0;
+}
