44acf2b1ac11cb47d9d64e5678f4f9dad3298051
gautamgc17 <71714942+gautamgc17@users.noreply.github.com>
2021-01-16 23:48:40 +0530

    Add files via upload

---
+++ myCaptain/ascii-values.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	for(int i=0 ; i<=255 ; i++){
+		cout<<"Ascii Value: "<< i << "\n"<<"Equivalent Character: "<< char(i) <<endl;
+	}
+	return 0;
+}
