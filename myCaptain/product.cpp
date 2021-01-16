faf81cf7152ee97b1165785239665cb19cbe4f22
gautamgc17 <71714942+gautamgc17@users.noreply.github.com>
2021-01-16 23:43:03 +0530

    Add files via upload

---
+++ myCaptain/product.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float x, y, product=1;
+	cout<<"Enter two numbers to find product\n";
+	cin>>x>>y;
+	product = x*y;
+	cout<<"The Product is: "<<product;
+	return 0;
+}
