36479fd966ba36d1272b4321ad510e1f8a262465
gautamgc17 <71714942+gautamgc17@users.noreply.github.com>
2021-01-16 07:23:48 +0530

    Update inline-functions.cpp

--- 4. dynamic memory allocation/inline-functions.cpp
+++ 4. dynamic memory allocation/inline-functions.cpp
@@ -1,3 +1,8 @@
+/*
+C++ provides an inline functions to reduce the function call overhead. Inline function is a function that is expanded in line when it is called. 
+When the inline function is called whole code of the inline function gets inserted or substituted at the point of inline function call. 
+This substitution is performed by the C++ compiler at compile time. Inline function may increase efficiency if it is small. */
+
 #include<iostream>
 using namespace std;
 
