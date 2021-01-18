c76400aa5115314df0cf794631d1beca09c33df5
gautamgc17 <71714942+gautamgc17@users.noreply.github.com>
2021-01-18 20:42:31 +0530

    Add files via upload

---
+++ 3.functions/printAllPrimes.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+// function to check whether a given number is prime or not
+bool isPrime(int n){
+	
+	for(int i=2; i<=n-1; i++){
+		if(n%i==0){
+			return false;
+		}
+	}
+	return true;
+}
+
+// function to print all prime numbers upto n - uing above function
+void printPrimes(int N){
+	
+	for(int i=2; i<=N; i++){
+		
+		if( isPrime(i) ){
+			cout<< i << " ";
+		}
+	}
+}
+
+int main(){
+	
+	int n;
+	cin>>n;
+	
+	cout<<"Prime numbers upto "<< n << " are: ";
+	printPrimes(n);
+}
