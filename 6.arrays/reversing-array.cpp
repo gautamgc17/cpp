e74a82ce19bcd811f3b2ca8ee400b54a9df59d3d
gautamgc17 <71714942+gautamgc17@users.noreply.github.com>
2021-02-24 00:17:15 +0530

    Add files via upload

---
+++ 6.arrays/reversing-array.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+void printArray(int arr[] , int n){
+	
+	for(int i=0; i<n; i++){
+		cout<<arr[i]<<"\t";
+	}
+}
+
+void reverseArray(int arr[] , int n){
+	
+	int i = 0 , j = n-1 , temp;
+	
+	while(i < j){
+		temp = arr[i];
+	    arr[i] = arr[j];
+	    arr[j] = temp;
+	    i++;
+	    j--;
+	}
+}
+
+int main(){
+	int arr[10] = {1,2,3,4,5};
+	
+	cout<<"Original Array:\n";
+	printArray(arr , 5);
+	
+	reverseArray(arr , 5);
+	cout<<"\nReversed Array:\n";
+	printArray(arr , 5);
+}
