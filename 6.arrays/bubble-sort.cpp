76917ff1722d7f944c7ab7589ca371c9ad2b8c31
gautamgc17 <71714942+gautamgc17@users.noreply.github.com>
2021-03-18 12:13:18 +0530

    Add files via upload

---
+++ 6.arrays/bubble-sort.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+void BubbleSort(int arr[] , int n){
+	for(int i=1; i<=n-1; i++){
+		
+		for(int j=0; j<=(n-1-i); j++){
+			
+			// pairwise swapping of elements in unsorted part of array
+			if(arr[j] > arr[j+1]){
+				swap(arr[j] , arr[j+1]);
+			}
+		}
+	}
+	
+}
+int main(){
+	int n ;
+	cin>>n;
+	
+	int arr[100];
+	for(int i=0; i<n; i++){
+		cin>>arr[i];
+	}
+	
+	BubbleSort(arr, n);
+	
+	for(int i=0; i<n; i++){
+		cout<<arr[i]<<" ";
+	}
+	
+	return 0;
+}
